012c9616c7f4bf686bb8beca08fca733bc6689da
Sam Burns <118425036@umail.ucc.ie>
2020-12-16 15:32:06 +0000

    original test for addColours()

--- tests/Main/ColourTest.java
+++ tests/Main/ColourTest.java
@@ -12,6 +12,12 @@ class ColourTest {
         Colour colour2 = new Colour("RGB",230,110,100);
         assertTrue(Colour.equalColours(colour1,colour2));
     }
+    @Test
+    public void addColours(Colour a, Colour b){
+        Colour colour1 = new Colour("RGB",100,180,15);
+        Colour colour2 = new Colour("RGB",80,50,90);
+        assertTrue(Colour.addColours(colour1,colour2));
+    }
 
 
 
