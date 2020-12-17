b5b32726db3b0c1e9c935e0fbf7034019805c7bd
Sam Burns <118425036@umail.ucc.ie>
2020-12-17 13:09:56 +0000

    Altered API to get rid of CMYK colour model and ensure there are exactly 3 components to each model, different models specified in a string

--- src/Main/Colour.java
+++ src/Main/Colour.java
@@ -5,7 +5,6 @@ public class Colour {
     private int w;
     private int x;
     private int y;
-    private int z;
 
     public Colour(int r, int g, int b) {
         this.type = "RGB";
@@ -17,17 +16,16 @@ public class Colour {
         limitInputsRGBY();
     }
 
-    public Colour(String type, int c, int m, int y, int k) {
+    public Colour(int r, int g, int b, String type) {
         this.type = type;
-        this.w = c;
-        this.x = m;
-        this.y = y;
-        this.z = k;
-        limitInputsCMYKw();
-        limitInputsCMYKx();
-        limitInputsCMYKy();
-        limitInputsCMYKz();
+        this.w = r;
+        this.x = g;
+        this.y = b;
+        limitInputsRGBW();
+        limitInputsRGBX();
+        limitInputsRGBY();
     }
+
 
     public void limitInputsRGBW() {
         if (this.w >= 255) {
@@ -36,6 +34,7 @@ public class Colour {
             this.w = 0;
         }
     }
+
     public void limitInputsRGBX() {
         if (this.x >= 255) {
             this.x = 255;
@@ -43,6 +42,7 @@ public class Colour {
             this.x = 0;
         }
     }
+
     public void limitInputsRGBY() {
         if (this.y >= 255) {
             this.y = 255;
@@ -51,110 +51,41 @@ public class Colour {
         }
     }
 
-    public void limitInputsCMYKw() {
-        if (this.w >= 100) {
-            this.w = 100;
-        } else if (this.w < 0) {
-            this.w = 0;
-        }
-    }
-    public void limitInputsCMYKx() {
-        if (this.x >= 100) {
-            this.x = 100;
-        } else if (this.x < 0) {
-            this.x = 0;
-        }
-    }
-    public void limitInputsCMYKy() {
-        if (this.y >= 100) {
-            this.y = 100;
-        } else if (this.y < 0) {
-            this.y = 0;
-        }
-    }
-    public void limitInputsCMYKz() {
-        if (this.z >= 100) {
-            this.z = 100;
-        } else if (this.z < 0) {
-            this.z = 0;
-        }
-    }
-
-    public int getFirstComponent(){
+    public int getFirstComponent() {
         return this.w;
     }
-    public int getSecondComponent(){
+
+    public int getSecondComponent() {
         return this.x;
     }
-    public int getThirdComponent(){
+
+    public int getThirdComponent() {
         return this.y;
-    }
-    public int getFourthComponent(){
-        return this.z;
     }
 
     public static boolean equalColours(Colour a, Colour b) {
-        if (a.type.equals("CMYK")) {
-            if (a.type.equals(b.type)) {
-                if (a.w == b.w) {
-                    if (a.x == b.x) {
-                        if (a.y == b.y) {
-                            if (a.z == b.z) {
-                                return true;
-                            }
-                        }
-                    }
-                }
+        if (a.type.equals(b.type)) {
+            if (a.w == b.w && a.x == b.x && a.y == b.y) {
+                return true;
             }
+            return false;
         }
-        else if (a.type.equals("RGB")) {
-                if (a.type.equals(b.type)) {
-                    if (a.w == b.w) {
-                        if (a.x == b.x) {
-                            if (a.y == b.y) {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
         return false;
     }
 
 
     public Object addColours(Colour a) {
-        if (a.type.equals("RGB")) {
-            if ((a.w + this.w) <= 255) {
-                if ((a.x + this.x) <= 255) {
-                    if ((a.y + this.y) <= 255) {
-                        Colour colour1 = new Colour((a.w + this.w), (a.x + this.x), (a.y + this.y));
-                        return colour1;
-                    }
-                }
-            }
-        }
-        else if (a.type.equals("CMYK")) {
-                if ((a.w + this.w) <= 100) {
-                    if ((a.x + this.x) <= 100) {
-                        if ((a.y + this.y) <= 100) {
-                            if ((a.z + this.z) <= 100) {
-                                Colour colour1 = new Colour("CMYK", (a.w + this.w), (a.x + this.x), (a.y + this.y), (a.z + this.z));
-                                return colour1;
-                        }
-                    }
-                }
-            }
+        if (a.type.equals(this.type)) {
+            return new Colour((a.w + this.w), (a.x + this.x), (a.y + this.y),(a.type));
         }
         return "These colours can't be added";
     }
 
     public String toString() {
-        if (this.type.equals("RGB")){
-            return String.format("Colour: Type -> RGB, R = %d, G = %d, B = %d", w, x, y);
-        }
-        return String.format("Colour: Type -> CMYK, C = %d, M = %d, Y = %d, k = %d", w, x, y,z);
+        return String.format("Colour: Type -> %s, R = %d, G = %d, B = %d", type,w, x, y);
     }
 }
+
 
 
 
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,10 +3,11 @@ package Main;
 public class Main {
     public static void main(String[] args){
         Colour a = new Colour(10,100,10);
-        Colour b = new Colour(200,100,150);
+        Colour b = new Colour(300,100,150);
+        System.out.println(a.toString());
         System.out.println(Colour.equalColours(a,b));
         System.out.println(a.addColours(b));
-        Colour c = new Colour(256,300,250);
+        Colour c = new Colour(256,300,250,"RGB");
         System.out.println(c.toString());
     }
 
