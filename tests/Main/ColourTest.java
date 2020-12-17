5e5cb9e8dd9ca846b5312795d1b00c5eb926b293
Sam Burns <118425036@umail.ucc.ie>
2020-12-17 13:38:51 +0000

    Made small changes to the tests

--- tests/Main/ColourTest.java
+++ tests/Main/ColourTest.java
@@ -9,14 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ColourTest {
 
     @Test
-    public void equalColours(Colour a, Colour b){
+    public void equalColours(){
         Colour colour1 = new Colour(230,110,100);
         Colour colour2 = new Colour(230,110,100);
         assertTrue(Colour.equalColours(colour1,colour2));
     }
 
     @Test
-    public void addColours(Colour a, Colour b){
+    public void addColours(){
         Colour colour1 = new Colour(100,180,15);
         Colour colour2 = new Colour(80,50,90);
         Colour colour3 = new Colour(180,230,105);
